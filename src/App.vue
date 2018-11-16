cdbeaf9c50176341ae2aea87a51b576e567e163c
ice48623 <ice48623@gmail.com>
2018-11-17 02:17:35 +0700

    initialized

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <tool-bar></tool-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ToolBar from '@/components/ToolBar'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      ToolBar
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+import Vuetify from 'vuetify'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#E53935', // #E53935
+    secondary: '#FFCDD2', // #FFCDD2
+    accent: '#3F51B5' // #3F51B5
+  }
+})
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app');
